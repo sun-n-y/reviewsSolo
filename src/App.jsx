64976fdeab7de8cb250c55e3d85d9dbd3ea2b2ef
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-26 10:18:57 -0400

    add person html and render

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
 import people from './data';
+import { useState } from 'react';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 function App() {
   const [person, setPerson] = useState(0);
   const { id, name, job, image, text } = people[person];
@@ -9,6 +10,20 @@ function App() {
         <div className="reviews-container">
           <div className="img-container">
             <img src={image} alt={name} className="person-img" />
+            <span className="quote-icon">
+              <FaQuoteRight />
+            </span>
+          </div>
+          <h4 className="person-name">{name}</h4>
+          <h3 className="person-job">{job}</h3>
+          <p className="text">{text}</p>
+          <div className="btn-container">
+            <button type="button" className="prev-btn">
+              <FaChevronLeft />
+            </button>
+            <button type="button" className="next-btn">
+              <FaChevronRight />
+            </button>
           </div>
         </div>
       </main>
